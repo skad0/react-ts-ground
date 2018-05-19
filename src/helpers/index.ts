6807862c71f56d72fb51ab4e05cbaa4ceb33bf09
Anton Krichevskii <mrskadi0@gmail.com>
2018-05-19 15:28:12 +0300

    pub

--- src/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-interface IRegExpContainer {
-    [key: string]: RegExp;
-};
-
-export const usedRegExp: IRegExpContainer = {
-    YOUTUBE_VIDEO_URL: /^http(s)?:\/\/([a-z0-9]+\.|)youtube.com(.*)\?v=([a-z0-9_-]+)/i
-};
