230926ab36c4631b37a889be2c0f974c4e4d0844
Anton Krichevskii <mrskadi0@gmail.com>
2018-05-20 12:23:43 +0300

    AddForm enzyme test

---
+++ src/AddForm/AddForm.test.tsx
@@ -0,0 +1,40 @@
+import * as enzyme from 'enzyme';
+import * as React from 'react';
+
+import AddForm from './index';
+
+describe('AddForm', () => {
+    it('should not call handle add if empty input', () => {
+        const handleAdd = jest.fn();
+        const wrapper = enzyme.shallow(<AddForm handleAdd={handleAdd} />);
+
+        wrapper.find('.AddForm-Button').simulate('click');
+
+        expect(handleAdd).not.toBeCalled();
+    });
+
+    it('should not call handle add if invalid input', () => {
+        const handleAdd = jest.fn();
+        const wrapper = enzyme.shallow(<AddForm handleAdd={handleAdd} />);
+
+        wrapper.find('.AddForm-Input').simulate('change', {
+            target: {value: 'https://google.com/'}
+        });
+        wrapper.find('.AddForm-Button').simulate('click');
+
+        expect(handleAdd).not.toBeCalled();
+    });
+
+    it('should call handle add if validated input', () => {
+        const handleAdd = jest.fn();
+        const wrapper = enzyme.shallow(<AddForm handleAdd={handleAdd} />);
+
+        wrapper.find('.AddForm-Input').simulate('change', {
+            target: {value: 'https://www.youtube.com/watch?v=OkwplGEUhkM'}
+        });
+        wrapper.find('.AddForm-Button').simulate('click');
+
+
+        expect(handleAdd).not.toBeCalled();
+    });
+});
